eff05cd1a5b1bccf325505c7af8309297fefcc42
juan <juan@juan-desktop>
2016-05-07 19:04:35 -0500

    juan

--- proyecto1/src/co/com/uniandes/main/Main.java
+++ proyecto1/src/co/com/uniandes/main/Main.java
@@ -1,33 +1,24 @@
 package co.com.uniandes.main;
 
-import java.io.File;
-import java.net.URL;
 import jpl.Query;
 
+/**
+ * @author juan
+ * Para ejecutar se de agregar los siguientes parametros
+ * Parametros de la maquina virtual: -Djava.library.path=/usr/lib/swi-prolog/lib/amd64
+ * Parametros del programa: ruta completa del archivo prolog
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		try {
-			
-			//"-Djava.library.path=/usr/bin/swipl";
-			String t1 = "consult('" + getAbsoluteSrcPath() + File.separator + "baseprolog.pl')";
-			System.out.println(getAbsoluteSrcPath());
-			Query q1 = new Query(t1);
-			System.out.println(t1 + " " + (q1.hasSolution() ? "succeeded" : "failed"));
-			
-			String t4 = "todos(X)";
-			Query q4 = new Query(t4);
+			Query q1 = new Query("consult('" + args[0] + "')");
+			System.out.println(q1.hasSolution() ? "Archivo cargado" : "Error");
+
+			Query q4 = new Query("todos(X)");
 			System.out.println(q4.oneSolution().get("X"));
-			System.out.println(q4.oneSolution().get("X").toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static String getAbsoluteSrcPath() {
-        URL url = Main.class.getResource("Main.class");
-        File f = new File(url.getPath()).getParentFile();
-        String srcPath = f.getAbsolutePath().replaceAll("%20", " ");
-        return srcPath;
-    }
 }
